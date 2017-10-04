c73647941b714ef7bde1082d186d672f5afb5bde
Emily Morgan <emorgan05@gmail.com>
2017-10-04 17:08:30 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,16 +2,22 @@
 require "nokogiri"
 require "pry"
 
+# projects = kickstarter.css("li.project.grid_4")
+# title = project.css("h2.bbcard_name strong a").text
+# img_src = project.css("div.project-thumbnail a img").attribute("src").value
+# description = project.css("p.bbcard_blurb").text
+# location = project.css(".location-name").text
+# percent_funded = project.css(".first.funded strong").text.gsub("%", "").to_i
+
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
-
   kickstarter = Nokogiri::HTML(html)
-  # projects = kickstarter.css("li.project.grid_4")
-  # title = project.css("h2.bbcard_name strong a").text
-  # img_src = project.css("div.project-thumbnail a img").attribute("src").value
-  # description = project.css("p.bbcard_blurb").text
-  # location = project.css(".location-name").text
-  # percent_funded = project.css(".first.funded strong").text.gsub("%", "").to_i
+
+  projects = {}
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
   binding.pry
 end
 
